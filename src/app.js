a7dd9364e2404e510ca00ebc9ac9c2afd49ec5e3
martadelpozo <138892528+martadelpozo@users.noreply.github.com>
2023-10-09 19:08:13 +0000

    Domain Name Generator

--- src/app.js
+++ src/app.js
@@ -9,3 +9,19 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronouns = ["the", "our", "you", "we", "me"];
+let adjectives = ["great", "happy", "wonderful", "delicious", "peaceful"];
+let nouns = ["jogger", "raccoon", "dog", "coffee", "flower"];
+let extensions = [".com", ".org", ".net", ".gov", ".edu"];
+
+for (let i = 0; i < pronouns.length; i++) {
+  for (let j = 0; j < adjectives.length; j++) {
+    for (let k = 0; k < nouns.length; k++) {
+      for (let l = 0; l < extensions.length; l++) {
+        let domainName = pronouns[i] + adjectives[j] + nouns[k] + extensions[l];
+        console.log(domainName);
+      }
+    }
+  }
+}
